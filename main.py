f7360e921a26985dcb44ad5e2eee3c594344d2a7
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-19 12:23:02 -0600

    using selenium to click elements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
@@ -8,9 +9,13 @@ options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 driver.get('https://en.wikipedia.org/wiki/Main_Page')
 
-article_count = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
-article_count.click()
-print(article_count.text)
+search = driver.find_element(By.NAME, value='search')
+search.send_keys('Python')
+search.send_keys(Keys.ENTER)
+# print(article_count.text)
+
+# all_portals = driver.find_element(By.LINK_TEXT, 'All portals')
+# all_portals.click()
 
 # price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
 # price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
@@ -22,4 +27,4 @@ print(article_count.text)
 # button = driver.find_element(By.CSS_SELECTOR, value='.a-price-whole')
 # print(button.text)
 
-driver.quit()
+# driver.quit()
