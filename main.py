6a7c58c71f2c57762563a0ce461df265c131b519
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-11-08 12:23:02 -0600

    setup webdriver

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
 
-response = driver.get('https://www.amazon.com')
 
-print(response)
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.amazon.com')
