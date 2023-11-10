f65a5600af153c03ffdaa7e57cbbc53b3d4120e8
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-11-10 12:23:02 -0600

    using selenium to scrape date data

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ options.add_experimental_option('detach', True)
 
 
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.amazon.com/Instant-Pot-Duo-Mini-Programmable/dp/B06Y1YD5W7/ref=sr_1_1?crid=3KMPX3FO5TROQ&keywords=instant%2Bpot&qid=1699949602&sprefix=instant%2Bpot%2Caps%2C116&sr=8-1&th=1')
+driver.get('https://www.python.org')
 
-price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
-price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
-print(f"The price is {price_dollar.text}.{price_cents.text}")
+dates = driver.find_elements(By.CSS_SELECTOR, value='.event-widget .shrubbery .menu li time')
+for date in dates:
+  print(date.text)
 
-search_bar = driver.find_element(By.NAME, value='q')
-print(search_bar.tag_name)
+# price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
+# price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+# # search_bar = driver.find_element(By.NAME, value='q')
+# # print(search_bar.get_attribute('placeholder'))
+
+# button = driver.find_element(By.CSS_SELECTOR, value='.a-price-whole')
+# print(button.text)
 
 driver.quit()
